9ef4958d8a453672cb180634b7783725f1d49c95
Sven ten Haaf <sventenhaaf@hotmail.com>
2016-04-19 19:00:46 +0200

    code up to lecture 27

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-// create a new component
-// should produce html
-
-// component should be put into the DOM somehow
-
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyB2OFPKrduRmIEyHGKcK7iUYYaaTgVh1NI';
 
-const App = () => {
-  return <div>Hi!</div>;
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        < SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
